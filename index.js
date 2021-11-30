4286a4a58975dc57483b6062681be7cd74023402
Jason Botesazan <jbotesazan@gmail.com>
2021-11-29 23:12:41 -0500

    commit tree 22d3531e2b6cac43fbecee2f1c5543000d894ce1

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "What Does The This Keyword Mean?",
+          "What Is The Constructor OO Pattern?",
+          "Implementing Blockchain Web API",
+          "The Test Driven Development Workflow",
+          "What Is NaN And How Can We Check For It",
+          "What Is The Difference Between StopPropagation And PreventDefault?",
+          "Immutable State And Pure Functions",
+          "What Is The Difference Between == And ===?",
+          "What Is The Difference Between Event Capturing And Bubbling?",
+          "What Is JSONP?"
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials;
 }
+
+// function getFirstLetters(str) {
+
+//   const firstLetters = str.split(' ').map(word => word[0]).join('');
+
+//   return firstLetters;
+// }
+
+// const titleCased = (array) => {
+
+//   const mapping = tutorials.map( function(element) {
+
+//     return element.charAt(0).toUpperCase() + element.slice(1)
+
+//   })
+
+//   return mapping
+  
+// }
+
+// const tutCapitalized = tutorials.charAt(0).toUpperCase() + tutorials.slice(1)
+
+// const mapping = tutorials.map( function(element) {
+//   return element.charAt(0).toUpperCase() + element.slice(1)
+// })
+
+// // console.log(mapping);
+
+// for (let i = 0; i < str.length; i++) {
+//   return str.charAt(i);
+// }
+
+// function getFirstLetters(str) {
+//   const firstLetters = str
+//     .split(' ')
+//     .map(word => word[0])
+//     .join('');
+
+//   return firstLetters;
+// }
